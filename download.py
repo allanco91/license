d2dfa830ade93add16adb7379fa1d53f7e20e0fa
Allan Caetano de Oliveira <allan.co@hotmail.com>
2018-04-05 08:50:27 -0300

    upload e download ok

--- download.py
+++ download.py
@@ -9,13 +9,11 @@ drive = GoogleDrive(gauth)
 
 # Cria uma lista com todos os arquivos do google drive
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+# Looping que percorre os arquivos da lista e faz download
+for file1 in file_list:
+    download_mimetype = None
+    mimetypes = ''
+    if file1['mimeType'] in mimetypes:
+        download_mimetype = mimetypes[file1['mimeType']]
 
-# Looping que percorre e apaga todos os arquivos
-for file2 in file_list:
-    file2.Delete()
-
-file1 = drive.CreateFile()
-# Lê o arquivos e seta o conteúdo para a instância
-file1.SetContentFile('upload_arq.txt')
-file1.Upload() # Faz o upload do arquivo.
-print('title: %s, mimeType: %s' % (file1['title'], file1['mimeType']))
+    file1.GetContentFile(file1['title'], mimetype=download_mimetype)
